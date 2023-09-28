1c4deea7d9cf144b272203e63ff54e229c2fc340
Javier Coronado <jacovi74@gmail.com>
2023-09-28 13:29:21 -0500

    se creo api y se creo el blueprint en la app de usuario

---
+++ api/usuario.py
@@ -0,0 +1,41 @@
+from flask import Blueprint, jsonify, request,json
+from config.db import db, app, ma
+from models.usuario import usuario, usuario_Schema,usuarios_Schema
+
+
+ruta_usuario = Blueprint("ruta_usuario",__name__)
+#routes_cliente = Blueprint("routes_cliente", __name__)
+
+
+
+@ruta_usuario.route("/usuario", methods=["GET"])
+def usuario():
+    resultall = usuario.query.all()
+    result = usuarios_Schema.dump(resultall)
+    return jsonify(result)
+
+@ruta_usuario.route("/usuario", methods=["POST"])
+def saveusuario():
+    id_usuario = request.json['id_usuario']
+    nombre=request.json['nombre']
+    new_usuario= usuario(id_usuario,nombre)
+    db.session.add(new_usuario)
+    db.session.commit()
+    return "Datos guardados con exitos"
+
+@ruta_usuario.route("/updausuario", methods=["PUT"])
+def updateusuario():
+    id_usuario = request.json['id_usuario']
+    nombre=request.json['nombre']
+    nusuario = usuario.query.get(id_usuario) #Select * from Cliente where id = id
+    nusuario.nombre=nombre 
+    db.session.commit()
+    return "Datos Actualizado con exitos"
+
+@ruta_usuario.route("/deleteusuario/<id>", methods=["DELETE"])
+def deleteusuario(id):
+    id_usuario = request.json['id_usuario']
+    usuariox = usuario.query.get(id_usuario)
+    db.session.delete(usuariox)
+    db.session.commit()
+    
--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from api.ciclovia import  ruta_ciclovia
 from api.alerta import ruta_alerta
 from api.ruta import ruta_ruta
 from api.lugar_estrategico import ruta_lugar_estrategico
-
+from api.usuario import ruta_usuario
 app.register_blueprint(ruta_ciclovia, url_prefix="/api")
 app.register_blueprint(ruta_alerta, url_prefix="/api")
 app.register_blueprint(ruta_ruta, url_prefix="/api")
 app.register_blueprint(ruta_lugar_estrategico, url_prefix="/api")
+app.register_blueprint(ruta_usuario, url_prefix="/api")
 
 
 #no me funcionan los llamados
